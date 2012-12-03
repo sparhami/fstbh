0c90eee5ebd75c2118b2a585bdbb8fd3cd53c970
Sepand Parhami <sepandparhami+github@gmail.com>
2012-12-03 08:02:24 -0600

    Initial commit

---
+++ chrome/content/fstbh.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function() {
+
+    let nav = document.getElementById('navigator-toolbox');
+    let toggler = document.getElementById('fullscr-toggler');
+    let wrapper = document.getElementById('com_sppad_fstbh_topChromeWrapper');
+
+    wrapper.appendChild(nav);
+    wrapper.appendChild(toggler);
+
+}, false);
